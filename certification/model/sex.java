9506d169f8f07255c42dfd83f231b34bf9bba954
MU <m.iouriev@gmail.com>
2024-04-18 11:59:09 +0700

    18.04

---
+++ certification/model/sex.java
@@ -0,0 +1,31 @@
+package model;
+
+public class sex extends User {
+
+    @Override
+    String NameSurnameLastName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'NameSurnameLastName'");
+    }
+
+    @Override
+    String dateOfBirth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dateOfBirth'");
+    }
+
+    @Override
+    Integer phoneNumb() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'phoneNumb'");
+    }
+
+    @Override
+    char sex() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sex'");
+    }
+
+    
+    
+}
